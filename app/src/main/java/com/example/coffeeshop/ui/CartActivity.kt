3fab6c685013b2e74b285c4875ed70c187a7d982
Monteiro Neto <montteiro.netto@gmail.com>
2025-10-18 12:49:20 -0300

    Cart Activity UI

---
+++ app/src/main/java/com/example/coffeeshop/ui/CartActivity.kt
@@ -0,0 +1,21 @@
+package com.example.coffeeshop.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.coffeeshop.R
+import com.example.coffeeshop.databinding.ActivityCartBinding
+
+class CartActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCartBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityCartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
